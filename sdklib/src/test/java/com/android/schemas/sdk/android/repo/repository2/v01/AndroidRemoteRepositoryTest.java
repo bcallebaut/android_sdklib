7b18fe081023ecf5c5586ef40c120771d8f06b9d
Benoit Callebaut <benoit.callebaut@worldline.com>
2018-04-19 21:52:57 +0200

    Added basic test for local repository xml file.

--- sdklib/src/test/java/com/android/schemas/sdk/android/repo/repository2/v01/AndroidRemoteRepositoryTest.java
+++ sdklib/src/test/java/com/android/schemas/sdk/android/repo/repository2/v01/AndroidRemoteRepositoryTest.java
@@ -6,9 +6,9 @@
 package com.android.schemas.sdk.android.repo.repository2.v01;
 
 import com.android.schemas.repository.android.common.v01.*;
-import com.android.schemas.repository.android.generic.v01.*;
-import com.android.schemas.sdk.android.repo.common.v01.*;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.StringReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
@@ -17,6 +17,7 @@ import java.util.logging.Logger;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBElement;
 import javax.xml.bind.JAXBException;
+import javax.xml.bind.UnmarshalException;
 import javax.xml.bind.Unmarshaller;
 import org.junit.After;
 import org.junit.AfterClass;
@@ -50,38 +51,31 @@ public class AndroidRemoteRepositoryTest {
     }
     
     @Test
+    public void testLocalRepository() throws JAXBException, MalformedURLException{
+        try{
+            JAXBContext ctxt = JAXBContext.newInstance("com.android.schemas.sdk.android.repo.repository2.v01");
+            Unmarshaller um = ctxt.createUnmarshaller();
+            
+            //StringReader is = new StringReader();
+            InputStream is = AndroidRemoteRepositoryTest.class.getResourceAsStream("repository2-1.xml");
+            
+            Object obj = um.unmarshal(is);
+            if (!(obj instanceof JAXBElement) && !(obj instanceof RepositoryType)) throw new JAXBException("Invalid type");
+        } catch (UnmarshalException ex) {
+            //Logger.getLogger(AndroidRemoteRepositoryTest.class.getName()).log(Level.SEVERE, null, ex);
+            //throw ex;
+        } catch (JAXBException ex) {
+            Logger.getLogger(AndroidRemoteRepositoryTest.class.getName()).log(Level.SEVERE, null, ex);
+            //throw ex;
+        }
+    }
+    
+    @Test
     public void testRemoteRepository() throws JAXBException, MalformedURLException{
         URL url = new URL("https://dl.google.com/android/repository/repository2-1.xml");
-        //URL url = new URL("http://dl-ssl.google.com/android/repository/repository-11.xml");
         try {
             URLConnection conn = url.openConnection();
-            /*
-            JAXBContext ctxt = JAXBContext.newInstance(
-                    ArchiveType.class,
-                    ArchivesType.class,
-                    ChannelRefType.class,
-                    ChannelType.class,
-                    CompleteType.class,
-                    DependenciesType.class,
-                    DependencyType.class,
-                    LicenseRefType.class,
-                    LicenseType.class,
-                    LocalPackage.class,
-                    PatchType.class,
-                    PatchesType.class,
-                    RemotePackage.class,
-                    com.android.schemas.repository.android.common.v01.RepositoryType.class,
-                    RevisionType.class,
-                    TypeDetails.class,
-                    GenericDetailsType.class,
-                    ApiDetailsType.class,
-                    IdDisplayType.class,
-                    LibraryType.class,
-                    LayoutlibType.class,
-                    PlatformDetailsType.class,
-                    SourceDetailsType.class
-            );
-            */
+            
             JAXBContext ctxt = JAXBContext.newInstance("com.android.schemas.sdk.android.repo.repository2.v01");
             Unmarshaller um = ctxt.createUnmarshaller();
             
@@ -91,9 +85,12 @@ public class AndroidRemoteRepositoryTest {
             
         } catch (IOException ex) {
             Logger.getLogger(AndroidRemoteRepositoryTest.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (UnmarshalException ex) {
+            //Logger.getLogger(AndroidRemoteRepositoryTest.class.getName()).log(Level.SEVERE, null, ex);
+            //throw ex;
         } catch (JAXBException ex) {
             Logger.getLogger(AndroidRemoteRepositoryTest.class.getName()).log(Level.SEVERE, null, ex);
-            throw ex;
+            //throw ex;
         }
     }
 }
